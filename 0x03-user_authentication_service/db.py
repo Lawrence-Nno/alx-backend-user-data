9ac56e6074b5577f2b9d2ee7959f75b7148d77fc
Lawrence Nno <lawrence.nno@gmail.com>
2023-08-15 11:51:47 +0100

    Edited db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """ DataBase Module to save and update database 
 """
+
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
-
 from user import Base, User
 
 
@@ -23,6 +24,7 @@ class DB:
         Base.metadata.create_all(self._engine)
         self.__session = None
 
+
     @property
     def _session(self) -> Session:
         """Memoized session object 
@@ -32,12 +34,14 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
+
     def add_user(self, email: str, hashed_password: str) -> User:
         """ This func Adds a new user to db """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
 
     def find_user_by(self, **kwargs) -> User:
         """ This func returns first row found in users table
@@ -50,6 +54,7 @@ class DB:
         if record is None:
             raise NoResultFound
         return record
+
 
     def update_user(self, user_id: int, **kwargs) -> None:
         """ This func finds user record and updates attributes """
